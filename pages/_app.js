34864d109d2f4ce982bc07b85ca475916af48955
Nick Dreckshage <nicholasdreckshage@gmail.com>
2021-11-28 01:17:51 -0600

    basic bookshop

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,26 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import {
+  ChakraProvider,
+  Heading,
+  Stack,
+  Container,
+  Link,
+} from "@chakra-ui/react";
+import NextLink from "next/link";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
+      <Container>
+        <Heading margin="10px 0">Bookshop Demo</Heading>
+        <Stack direction="row" spacing="5px">
+          <NextLink href="/">
+            <Link>Home</Link>
+          </NextLink>
+          <NextLink href="/blog">
+            <Link>Blog</Link>
+          </NextLink>
+        </Stack>
+      </Container>
       <Component {...pageProps} />
     </ChakraProvider>
   );
