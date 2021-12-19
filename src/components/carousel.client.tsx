004350596e5d956c923dcd99eafe535bc6ad6ee8
Nick Dreckshage <nicholasdreckshage@gmail.com>
2021-12-19 10:17:10 -0600

    snap ios fix

--- src/components/carousel.client.tsx
+++ src/components/carousel.client.tsx
@@ -16,7 +16,7 @@ const Carousel = () => {
   return (
     <div className="container px-10">
       <div
-        className="relative w-full flex gap-6 snap-x overflow-x-auto pb-14 scroll-smooth snap-always"
+        className="relative w-full flex gap-6 overflow-x-auto pb-14 scroll-smooth snap-x snap-always snap-mandatory"
         ref={containerRef}
       >
         {[
