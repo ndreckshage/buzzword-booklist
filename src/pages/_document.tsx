7847496c6c550eabe25a13f196f054fff83e44f9
Nick Dreckshage <nicholasdreckshage@gmail.com>
2022-01-25 00:58:37 -0600

    add better bold

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,7 @@ export default function Document() {
           crossOrigin="true"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Merriweather:wght@700&family=Montserrat&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Merriweather:wght@700&family=Montserrat:wght@400;700&display=swap"
           rel="stylesheet"
         />
       </Head>
